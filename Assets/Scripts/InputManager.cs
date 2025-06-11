c170b90801a0ad6f84a2b0e1a9fb70d698aed84a
TateEller <tce22a@acu.edu>
2025-06-11 18:02:50 -0500

    Mobile Build Controls
    I think I had to recreate the repository so this may be the first/only history

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private Camera sceneCamera;
+
+    private Vector3 lastPosition;
+
+    [SerializeField] private LayerMask placementLayermask;
+
+    public event Action<Vector3> OnStartPlacement;
+    public event Action<Vector3> OnDragPlacement;
+    public event Action<Vector3> OnEndPlacement;
+    private bool isDragging = false;
+
+    private void Update()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE
+        if (Input.GetMouseButtonDown(0) && !IsPointerOverUI())
+        {
+            Vector3 pos = GetWorldPosition(Input.mousePosition);
+            OnStartPlacement?.Invoke(pos);
+            isDragging = true;
+        }
+        else if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            Vector3 pos = GetWorldPosition(Input.mousePosition);
+            OnEndPlacement?.Invoke(pos);
+            isDragging = false;
+        }
+        else if (Input.GetMouseButton(0) && isDragging)
+        {
+            Vector3 pos = GetWorldPosition(Input.mousePosition);
+            OnDragPlacement?.Invoke(pos);
+        }
+#elif UNITY_IOS || UNITY_ANDROID
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = touch.position;
+
+            if (touch.phase == TouchPhase.Began && !IsPointerOverUI(touch.fingerId))
+            {
+                Vector3 pos = GetWorldPosition(touchPos);
+                OnStartPlacement?.Invoke(pos);
+                isDragging = true;
+            }
+            else if ((touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) && isDragging)
+            {
+                Vector3 pos = GetWorldPosition(touchPos);
+                OnEndPlacement?.Invoke(pos);
+                isDragging = false;
+            }
+            else if (touch.phase == TouchPhase.Moved && isDragging)
+            {
+                Vector3 pos = GetWorldPosition(touchPos);
+                OnDragPlacement?.Invoke(pos);
+            }
+        }
+#endif
+    }
+
+    public Vector3 GetWorldPosition(Vector2 screenPosition)
+    {
+        Vector3 screenPos = new Vector3(screenPosition.x, screenPosition.y, sceneCamera.nearClipPlane);
+        Ray ray = sceneCamera.ScreenPointToRay(screenPos);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100, placementLayermask))
+        {
+            return hit.point;
+        }
+        return lastPosition;
+    }
+
+    public bool IsPointerOverUI()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        if (Input.touchCount > 0)
+        {
+            return EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
+        }
+#endif
+        return EventSystem.current.IsPointerOverGameObject();
+    }
+}
---
+++ Assets/Scripts/PlacementSystem.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject mouseIndicator;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private Grid grid;
+
+    [SerializeField] private ObjectsDatabaseSO database;
+    private int selectedObjectIndex = -1;
+
+    [SerializeField] private GameObject gridVisualization;
+
+    private GridData floorData, furnitureData;
+
+    private List<GameObject> placedGameObjects = new();
+
+    [SerializeField] private PreviewSystem preview;
+
+    private Vector3Int lastDetectedPosition = Vector3Int.zero;
+
+    private bool canPlace = false;
+    private bool isDragging = false;
+    private Vector3 startTouchPosition;
+
+    private void Start()
+    {
+        StopPlacement();
+        floorData = new();
+        furnitureData = new();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+
+        selectedObjectIndex = database.objectsData.FindIndex(data => data.ID == ID);
+        if (selectedObjectIndex < 0)
+        {
+            Debug.LogError($"No ID found {ID}");
+            return;
+        }
+        canPlace = false;
+
+        gridVisualization.SetActive(true);
+        preview.StartShowingPlacementPreview(database.objectsData[selectedObjectIndex].Prefab, database.objectsData[selectedObjectIndex].Size);
+
+        inputManager.OnStartPlacement += HandleStartPlacement;
+        inputManager.OnDragPlacement += HandleDragPlacement;
+        inputManager.OnEndPlacement += HandleEndPlacement;
+
+        StartCoroutine(EnablePlacementNextFrame());
+    }
+
+    private IEnumerator EnablePlacementNextFrame()
+    {
+        yield return null;
+        canPlace = true;
+    }
+
+    private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
+    {
+        GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ? floorData : furnitureData;  //works for tutorial. ID zero will be abke to be placed on others
+
+        return selectedData.CanPlaceObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size);
+    }
+    private void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+
+        gridVisualization.SetActive(false);
+        preview.StopShowingPreview();
+
+        inputManager.OnStartPlacement -= HandleStartPlacement;
+        inputManager.OnDragPlacement -= HandleDragPlacement;
+        inputManager.OnEndPlacement -= HandleEndPlacement;
+
+        lastDetectedPosition = Vector3Int.zero;
+    }
+
+    private void HandleStartPlacement(Vector3 worldPos)
+    {
+        if (!canPlace) return;
+
+        isDragging = false;
+        startTouchPosition = worldPos;
+
+        UpdatePreviewPosition(worldPos);
+
+        //Display preview at center
+        Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenter);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100, LayerMask.GetMask("PlacementLayer")))
+        {
+            Vector3Int gridPos = grid.WorldToCell(hit.point);
+            preview.UpdatePosition(grid.CellToWorld(gridPos), CheckPlacementValidity(gridPos, selectedObjectIndex));
+        }
+    }
+    private void HandleDragPlacement(Vector3 worldPos)
+    {
+        if (!canPlace) return;
+
+        //if finger moveed far enough, start dragging
+        if (Vector3.Distance(worldPos, startTouchPosition) > 0.1f)
+        {
+            isDragging = true;
+        }
+        UpdatePreviewPosition(worldPos);
+    }
+    private void HandleEndPlacement(Vector3 worldPos)
+    {
+        if (!canPlace) return;
+
+        if (!isDragging)
+        {
+            //tap to place (no dragging)
+            PlaceBuildingAt(startTouchPosition);
+        }
+        else
+        {
+            //drag to place at release
+            PlaceBuildingAt(worldPos);
+        }
+
+        StopPlacement();
+    }
+    private void UpdatePreviewPosition(Vector3 worldPos)
+    {
+        Vector3Int gridPosition = grid.WorldToCell(worldPos);
+        if (lastDetectedPosition != gridPosition)
+        {
+            bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+            mouseIndicator.transform.position = worldPos;
+            preview.UpdatePosition(grid.CellToWorld(gridPosition), placementValidity);
+            lastDetectedPosition = gridPosition;
+        }
+    }
+    private void PlaceBuildingAt(Vector3 worldPos)
+    {
+        Vector3Int gridPosition = grid.WorldToCell(worldPos);
+        if (!CheckPlacementValidity(gridPosition, selectedObjectIndex)) return;
+
+        GameObject newObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab);
+        newObject.transform.position = grid.CellToWorld(gridPosition);
+        placedGameObjects.Add(newObject);
+
+        GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ? floorData : furnitureData;  //works for tutorial. ID zero will be abke to be placed on others
+        selectedData.AddObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size, database.objectsData[selectedObjectIndex].ID, placedGameObjects.Count - 1);
+
+        preview.UpdatePosition(grid.CellToWorld(gridPosition), false);
+    }
+}
